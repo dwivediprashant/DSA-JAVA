21fe3ac325047f1e13ded9f51f2c984dd7787cbe
Prashant <gagandwivedi0101@gmail.com>
2025-09-27 20:02:31 +0530

    DAY-8: Search in sorted rotated array using binary search(O(logn)) #leetcode33(medium)

---
+++ practice-standard-questions/searchInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class searchInRotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        int mid;
+        
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(target<=nums[high] && target>nums[mid]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int[] nums={4,5,6,7,0,1,2};
+        int target=2;
+        int idx=search(nums,target);
+        System.out.print(idx==-1? target +" not found ! " : target+ " found at index "+idx); 
+    }
+    
+}
