c55468c5e3d3259b247c9bfafc0310a1da8712a7
Prashant <gagandwivedi0101@gmail.com>
2025-09-29 23:17:24 +0530

    DAY-10: Matrix fundamental & print spiral matrix

---
+++ 2dArray/Matrix.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Matrix {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int[][] matrix=new int[3][3];
+        int rows=matrix.length;
+        int col=matrix[0].length;
+        System.out.print("Enter values : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Values are : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ 2dArray/MaxMin.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MaxMin {
+    public static void maxMin(int[][] matrix){
+        int max=matrix[0][0];
+        int min=matrix[0][0];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]>max){
+                    max=matrix[i][j];
+                }
+                if(matrix[i][j]<min){
+                    min=matrix[i][j];
+                }
+            }
+        }
+        System.out.println("Maximum : " + max);
+        System.out.println("Minimum : " + min);
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int[][] matrix={{4,5,3},{5,-9,7},{1,5,4}};
+        maxMin(matrix);
+    }
+    
+}
