ceb044fed2a578a680b0256218513bf7d3b835f9
Prashant <gagandwivedi0101@gmail.com>
2025-10-24 08:36:33 +0530

    DAY-21: Recursion(Friends Pairing problem)

---
+++ Recursion/Pairing.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Pairing {
+    public static int pair(int n){
+        
+        return (n-1)*pairFriendsWays(n-2);
+    }
+    public static int single(int n){
+        
+        return pairFriendsWays(n-1);
+    }
+    public static int pairFriendsWays(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+       
+        return single(n)+pair(n);
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=5;
+        System.out.print(pairFriendsWays(n));
+    }
+    
+}
