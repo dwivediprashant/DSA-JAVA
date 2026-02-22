0b63b9cf3a64ab71057079523d416e2544011696
dwivediprashant <prashantdwivedi.0219@gmail.com>
2026-02-22 20:29:19 +0530

    DAY-33 : Add at given position + print size of linked list

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,5 +1,8 @@
 package LinkedList;
 public class LinkedList{
+    //head pointer
+    public Node head;
+    public int size;
     static class Node{
         int data;
         Node next;
@@ -8,12 +11,12 @@ public class LinkedList{
             this.next=null;
         }
     }
-    public Node head;
 
     //Methods
     //Insert at begin
     void addAtBegin(int data){
         Node node=new Node(data);
+        size++;
         //empty linkedlist case
         if(head==null){
             head=node;
@@ -25,6 +28,7 @@ public class LinkedList{
     //add at last
     void addAtLast(int data){
         Node node=new Node(data);
+        size++;
         if(head==null){
             head=node;
         }else{
@@ -33,6 +37,35 @@ public class LinkedList{
             while(temp.next!=null){
                 temp=temp.next;
             }
+            temp.next=node;
+        }
+    }
+    //add at given position
+    void addAtPosition(int pos,int data){
+        if(pos==0){
+            addAtBegin(data);
+            return;
+        }
+        if(pos<0){
+            System.out.println("Position should be in between 0 to size of linked list .");
+            return;
+        }
+        Node node=new Node(data);
+        size++;
+        //empty linkedlist
+        if(head==null){
+            head=node;
+        }else{
+            //traverse to the given position
+            Node temp=head;
+            for(int i=0;i<=pos-2;i++){
+                temp=temp.next;
+                if(temp==null){
+                    System.out.println("Position should be in between 0 to size of linked list .");
+                    return;
+                }
+            }
+            node.next=temp.next;
             temp.next=node;
         }
     }
@@ -52,6 +85,8 @@ public class LinkedList{
         ll.addAtBegin(30);
         ll.addAtLast(40);
         ll.addAtLast(40);
+        ll.addAtPosition(0, 90);
         ll.printLinkedList();
+        System.out.println(ll.size);
     }
 }
