92d9bdd69e7c601ab935b715e372456f943bc9a8
dwivediprashant <prashantdwivedi.0219@gmail.com>
2026-03-05 16:47:26 +0530

    DAY-35 : Solved reverse string using stack

---
+++ Stack/questions/q2.java
@@ -0,0 +1,25 @@
+package Stack.questions;
+import java.util.Stack;
+//que : reverse string using stack
+public class q2 {
+
+    static void reverse(String str, Stack<Character> s,int idx,StringBuilder rev){
+        if(idx==str.length()){
+            return;
+        }
+        char currChar=str.charAt(idx);
+        s.push(currChar);
+        reverse(str,s,idx+1,rev);
+
+        rev.append(currChar);
+
+    }
+    public static void main(String[] args) {
+        String str="prashant";
+        Stack<Character> s=new Stack<>();
+        StringBuilder rev=new StringBuilder("");
+        reverse(str, s,0,rev);
+
+        System.out.println(rev);
+    }
+}
