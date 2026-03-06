bc7e09a45b9b2cf95181c5d981eba9d65b522c5d
dwivediprashant <prashantdwivedi.0219@gmail.com>
2026-03-06 12:05:18 +0530

    DAY-36 : Solved reverse stack problem [space : O(n) time : O(n)]

---
+++ Stack/questions/q3.java
@@ -0,0 +1,33 @@
+package Stack.questions;
+import java.util.Stack;
+public class q3 {
+
+    public static Stack<Integer> reverseStack(Stack<Integer> s){
+        Stack<Integer> revS=new Stack<>();
+
+        while(!s.isEmpty()){
+            int top=s.peek();
+            revS.push(top);
+            s.pop();
+        }
+        return revS;
+    }
+    public static void printStack(Stack<Integer> st){
+        while (!st.isEmpty()) {
+            System.out.println(st.pop());
+        }
+        // System.out.println("print completed");
+    }
+    public static void main(String[] args) {
+        Stack <Integer> s=new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        //original stack
+        // printStack(s);
+        //reverse stack fxn
+        Stack<Integer> revS=reverseStack(s);
+        printStack(revS);
+    }
+}
